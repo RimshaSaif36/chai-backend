35030c0180c3c791139709ee38d07692a08fbe26
Rimsha <rimsha-email@example.com>
2025-05-21 07:47:15 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@
  dotenv.config({path: './env'})
 
  connectDB()
+ .then(() =>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`o Server is running ar port : ${process.env.PORT}`);
+    })
+ })
+ .catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
+ })
 
  /*
  import express from "express"
